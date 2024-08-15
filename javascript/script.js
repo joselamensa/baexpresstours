1afb99ed117e94b2800d24162cae0c5dc028eebe
Jose Lamensa <joselamensa10@gmail.com>
2024-08-15 19:16:45 -0300

    agrego exp

--- javascript/script.js
+++ javascript/script.js
@@ -35,3 +35,50 @@ function changeLanguage(language) {
   url.searchParams.set("lang", language);
   window.location.href = url.href;
 }
+
+// IMAGENES EXPERIENCIAS
+
+var slides = document.querySelectorAll(".slide");
+var dots = document.querySelectorAll(".dot");
+var index = 0;
+
+
+function prevSlide(n){
+  index+=n;
+  console.log("prevSlide is called");
+  changeSlide();
+}
+
+function nextSlide(n){
+  index+=n;
+  changeSlide();
+}
+
+changeSlide();
+
+function changeSlide(){
+    
+  if(index>slides.length-1)
+    index=0;
+  
+  if(index<0)
+    index=slides.length-1;
+  
+  
+  
+    for(let i=0;i<slides.length;i++){
+      slides[i].style.display = "none";
+      
+      dots[i].classList.remove("active");
+      
+      
+    }
+    
+    slides[index].style.display = "block";
+    dots[index].classList.add("active");
+
+  
+
+}
+
+
